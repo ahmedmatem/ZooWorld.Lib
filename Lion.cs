7e81910fd37a81e61a6153395de055b0e8535f59
Ahmed Matem <ahmedmatem@gmail.com>
2025-11-06 10:42:08 +0200

    CHange Lion sound

--- Lion.cs
+++ Lion.cs
@@ -3,5 +3,5 @@ namespace ZooWorld.Lib;
 public class Lion : Animal
 {
     public Lion(string name, int age) : base(name, age) { }
-    public override string MakeSound() => "Roar!";
+    public override string MakeSound() => "Roar - roar!";
 }
