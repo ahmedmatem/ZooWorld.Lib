a016a1782d0ce899b589e37281e75fdc5b93499c
Ahmed Matem <ahmedmatem@gmail.com>
2025-11-07 12:16:47 +0200

    Simplify lion's roar sound in MakeSound method
    Updated the `MakeSound` method in the `Lion` class to return "Roar!" instead of "Roar - roar!". This change simplifies the representation of the lion's roar for clarity and consistency.

--- Lion.cs
+++ Lion.cs
@@ -3,5 +3,5 @@ namespace ZooWorld.Lib;
 public class Lion : Animal
 {
     public Lion(string name, int age) : base(name, age) { }
-    public override string MakeSound() => "Roar - roar!";
+    public override string MakeSound() => "Roar!";
 }
